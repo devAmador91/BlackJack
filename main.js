81cf4add8fa9033b813cfb1803f6c5686bb3b3f1
Amador Aguilar <amador.aguilar@valtech.com>
2023-07-03 14:00:26 -0600

    configuracion css

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ document.querySelector('#app').innerHTML = `
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Blackjack</title>
-<link rel="stylesheet" href="./assets/Css/styles.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
